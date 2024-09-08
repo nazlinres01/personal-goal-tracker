413b6a1906070bcf09a588e6eea4342c2e71c8df
Nazlı Nur ESMERAY <nazlinuresmeray049@gmail.com>
2024-09-08 19:29:13 +0300

    Data added

---
+++ backend/PersonalGoalTrackerAPI/data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalGoalTrackerAPI.Models;
+
+namespace PersonalGoalTrackerAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Goal> Goals { get; set; }
+    }
+}
