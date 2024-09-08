1e39013c0cf6d9d6ecfafe76f3445ed772201d1a
Nazlı Nur ESMERAY <nazlinuresmeray049@gmail.com>
2024-09-08 13:13:34 +0300

    goalItem added

---
+++ frontend/personal-goal-tracker/src/components/GoalItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './GoalItem.css';
+
+function GoalItem({ goal }) {
+  return (
+    <div className="goal-item">
+      <h2>{goal.title}</h2>
+      <p>{goal.description}</p>
+      <span>Due Date: {goal.dueDate}</span>
+    </div>
+  );
+}
+
+export default GoalItem;
