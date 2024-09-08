006cac5e0adb2feb6fe9373a4938ebdea04cac8d
Nazlı Nur ESMERAY <nazlinuresmeray049@gmail.com>
2024-09-08 13:12:41 +0300

    header added

---
+++ frontend/personal-goal-tracker/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Personal Goal Tracker</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/goals">Goals</a></li>
+          <li><a href="/create-goal">Create Goal</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
