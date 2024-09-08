056843f70ddb654671dc346e3c7a926a7e087fc5
Nazlı Nur ESMERAY <nazlinuresmeray049@gmail.com>
2024-09-08 19:28:03 +0300

    Repositories added

---
+++ backend/PersonalGoalTrackerAPI/Repositories/GoalRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PersonalGoalTrackerAPI.Data;
+using PersonalGoalTrackerAPI.Models;
+
+namespace PersonalGoalTrackerAPI.Repositories
+{
+    public class GoalRepository : IGoalRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GoalRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Tüm hedefleri getir
+        public async Task<IEnumerable<Goal>> GetAllGoalsAsync()
+        {
+            return await _context.Goals.ToListAsync();
+        }
+
+        // ID'ye göre hedef getir
+        public async Task<Goal> GetGoalByIdAsync(int id)
+        {
+            return await _context.Goals.FindAsync(id);
+        }
+
+        // Yeni hedef ekle
+        public async Task AddGoalAsync(Goal goal)
+        {
+            await _context.Goals.AddAsync(goal);
+            await _context.SaveChangesAsync();
+        }
+
+        // Hedefi güncelle
+        public async Task UpdateGoalAsync(Goal goal)
+        {
+            _context.Goals.Update(goal);
+            await _context.SaveChangesAsync();
+        }
+
+        // Hedefi sil
+        public async Task DeleteGoalAsync(int id)
+        {
+            var goal = await _context.Goals.FindAsync(id);
+            if (goal != null)
+            {
+                _context.Goals.Remove(goal);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ backend/PersonalGoalTrackerAPI/Repositories/IGoalRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PersonalGoalTrackerAPI.Models;
+
+namespace PersonalGoalTrackerAPI.Repositories
+{
+    public interface IGoalRepository
+    {
+        Task<IEnumerable<Goal>> GetAllGoalsAsync();  // Tüm hedefleri getir
+        Task<Goal> GetGoalByIdAsync(int id);  // ID'ye göre hedef getir
+        Task AddGoalAsync(Goal goal);  // Yeni hedef ekle
+        Task UpdateGoalAsync(Goal goal);  // Hedefi güncelle
+        Task DeleteGoalAsync(int id);  // Hedefi sil
+    }
+}
